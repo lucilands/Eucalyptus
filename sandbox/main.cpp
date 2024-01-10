ab5da4c8524c4db075d89d242b00a02bd9be95d3
TheHerowither <lucland278@gmail.com>
2024-01-10 16:11:38 +0100

    [Sandbox] Add audio demo

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -12,11 +12,14 @@ int main(){
     //Eucalyptus::Shader shader = Eucalyptus::Shader::LoadFromFile(".\\shaders\\vertex.vs", ".\\shaders\\fragment.fs");
     Eucalyptus::Mesh tree = Eucalyptus::LoadMeshFromOBJ("models\\tree-tex.obj");
 
+    Eucalyptus::AudioClip audio("sound/Juhani Junkala [Retro Game Music Pack] Level 1.wav");
+
     Eucalyptus::MeshRenderer renderer(tree, Eucalyptus::Shader::Lit());
 
     renderer.transform.Scale({0.4f, 0.4f, 0.4f});
     renderer.transform.Translate({0.0f, -2.0f, 0.0f});
 
+    audio.play();
     while (window.IsOpen()){
         window.Clear();
 
