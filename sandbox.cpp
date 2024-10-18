ba9c1ce599493ae383b5f8a36dd792c933813c60
lucilands <lucland278@gmail.com>
2024-10-18 20:06:40 +0200

    Update sandbox

--- sandbox.cpp
+++ sandbox.cpp
@@ -8,22 +8,29 @@ int main() {
     Eucalyptus::Window window({1080, 720}, "Demo window");
 
     Eucalyptus::Scene main_scene;
-    Eucalyptus::Object cube;
+    Eucalyptus::Object monkey;
+    Eucalyptus::Object sphere;
 
     Eucalyptus::Shader basic_shader = Eucalyptus::Shader::FromFile("assets/shader/vert.vs", "assets/shader/frag.fs");
     Eucalyptus::Material basic(basic_shader, Eucalyptus::Texture("assets/texture/brick.jpg"));
 
-    Eucalyptus::Model model(basic, Eucalyptus::LoadGLBmesh("assets/models/sphere.glb"));
+    Eucalyptus::Model monkey_model(basic, Eucalyptus::LoadGLBmesh("assets/models/monkey.glb"));
+    Eucalyptus::Model sphere_model(basic, Eucalyptus::LoadGLBmesh("assets/models/sphere.glb"));
 
-    cube.AddComponent<Eucalyptus::ModelRenderer>(model);
+    monkey.AddComponent<Eucalyptus::ModelRenderer>(monkey_model);
+    sphere.AddComponent<Eucalyptus::ModelRenderer>(sphere_model);
 
-    main_scene.AddObject(&cube);
+    main_scene.AddObject(&monkey);
+    main_scene.AddObject(&sphere);
+    main_scene.GetCamera()->position = {0.0f, 0.0f, 10.0f};
+
+    sphere.GetComponent<Eucalyptus::Transform>()->Translate({3.0f, 0.0f, 0.0f});
 
     main_scene.Awake();
     while (window.IsRunning()) {
         window.Clear(Eucalyptus::Colors::DarkGray);
 
-        cube.GetComponent<Eucalyptus::Transform>()->Rotate(10.0f * window.delta_time, {1.0, 1.0, 0.0});
+        monkey.GetComponent<Eucalyptus::Transform>()->Rotate(10.0f * window.delta_time, {1.0, 1.0, 0.0});
         
         main_scene.Update();
         window.Update();
